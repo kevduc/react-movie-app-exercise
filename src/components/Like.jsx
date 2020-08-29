6c4322260bd0de85d3febac3d820fc6fd41bc9a3
KevDuc <kduconge@gmail.com>
2020-08-29 02:19:36 +0200

    Like working

---
+++ src/components/Like.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
+
+function Like(props) {
+  return (
+    <button className={`btn no-border ${props.className ? props.className : ""}`} onClick={props.onClick}>
+      {props.liked ? (
+        <FontAwesomeIcon icon={fasHeart} style={{ color: props.colorLiked }} />
+      ) : (
+        <FontAwesomeIcon icon={farHeart} style={{ color: props.color }} />
+      )}
+    </button>
+  );
+}
+
+export default Like;
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
+
+import Like from "./Like";
+
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
   state = { movies: getMovies() };
 
+  handleLike = (movieId) => {
+    const movies = [...this.state.movies];
+    const movieIndex = this.state.movies.findIndex((movie) => movie._id === movieId);
+    movies[movieIndex] = { ...movies[movieIndex] };
+    movies[movieIndex].liked = !movies[movieIndex].liked;
+    this.setState({ ...this.state, movies });
+  };
+
   handleDelete = (movieId) => {
     const movies = this.state.movies.filter((movie) => movie._id !== movieId);
-    this.setState({ movies });
+    this.setState({ ...this.state, movies });
   };
 
   render() {
@@ -17,30 +28,44 @@ class Movies extends Component {
           {numberOfMovies > 0 ? `Showing ${numberOfMovies} movies in the database.` : `There are no movies in the database.`}
         </p>
         {numberOfMovies > 0 ? (
-          <table className="table table-hover mt-5 align-middle">
+          <table className="table table-hover mt-5 align-middle text-center">
             <thead>
               <tr>
-                <th scope="col">Title</th>
+                <th scope="col" className="text-left">
+                  Title
+                </th>
                 <th scope="col">Genre</th>
                 <th scope="col">Stock</th>
                 <th scope="col">Rate</th>
+                <th scope="col"></th>
                 <th scope="col"></th>
               </tr>
             </thead>
             <tbody>
               {this.state.movies.map((movie) => (
                 <tr key={movie._id}>
-                  <td>{movie.title}</td>
+                  <td className="text-left">{movie.title}</td>
                   <td>{movie.genre.name}</td>
                   <td>{movie.numberInStock}</td>
                   <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      className="btn-lg"
+                      liked={movie.liked}
+                      //color="black"
+                      //colorLiked="red"
+                      onClick={() => {
+                        this.handleLike(movie._id);
+                      }}
+                    />
+                  </td>
                   <td>
                     <button
                       onClick={() => {
                         this.handleDelete(movie._id);
                       }}
                       type="button"
-                      className="btn btn-danger btn-lg">
+                      className="btn btn-danger">
                       Delete
                     </button>
                   </td>
