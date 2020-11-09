8558507608926061476c4e8148db8d2664a3c085
KevDuc <kduconge@gmail.com>
2020-11-09 23:21:42 +0000

    Better StarRating code

--- src/components/common/StarRating.jsx
+++ src/components/common/StarRating.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
-import { faStar as fasStar } from "@fortawesome/free-solid-svg-icons";
-import { faStarHalf } from "@fortawesome/free-solid-svg-icons";
-import { faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
+import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
+import { faStar as fullStar } from "@fortawesome/free-solid-svg-icons";
+import { faStarHalf as halfStarNoBorder } from "@fortawesome/free-solid-svg-icons";
+import { faStarHalfAlt as halfStarBorder } from "@fortawesome/free-solid-svg-icons";
 
 const StarRating = (props) => {
-  const STAR_ICONS = [farStar, props.hideEmpty ? faStarHalf : faStarHalfAlt, fasStar];
+  const { rating, maxRating, hideEmptyStars, className, color } = props;
+
+  const fullStarCount = Math.floor(rating);
+  const halfStar = hideEmptyStars ? halfStarNoBorder : halfStarBorder;
+
+  const hasHalf = rating % 1 !== 0;
+
+  const stars = new Array(maxRating).fill(emptyStar).fill(fullStar, 0, fullStarCount);
+
+  if (hasHalf) stars[fullStarCount] = halfStar;
+
   return (
     <div className="text-nowrap">
-      {new Array(props.maxRating)
-        .fill(0)
-        .fill(1, 0, Math.floor(props.rating))
-        .fill(0.5, Math.floor(props.rating), Math.ceil(props.rating))
-        .map((value, index) => (
-          <FontAwesomeIcon
-            key={index}
-            className={`${props.className} ${props.hideEmpty && value === 0 ? "invisible" : "visible"} fa-w-18`}
-            icon={STAR_ICONS[2 * value]}
-            style={{ color: props.color }}
-          />
-        ))}
+      {stars.map((star, index) => (
+        <FontAwesomeIcon
+          key={index}
+          className={`${className} ${hideEmptyStars && star === emptyStar ? "invisible" : "visible"} fa-w-18`}
+          icon={star}
+          style={{ color }}
+        />
+      ))}
     </div>
   );
 };
